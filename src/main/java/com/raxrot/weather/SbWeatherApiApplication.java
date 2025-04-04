a5478ffc63ab14a7a63f19176c7e1ece6849e7be
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-04 15:44:01 +0100

    init

---
+++ src/main/java/com/raxrot/weather/SbWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbWeatherApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raxrot/weather/SbWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
